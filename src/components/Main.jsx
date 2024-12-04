058e28295e416e021fac8846ae94f64e62a3e879
MyUsername <rosanio.c@gmail.com>
2024-12-04 14:01:39 +0100

    creazione SECTIONS statiche di base

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,31 @@
 import { useState } from 'react';
-import Articles from '../data/Articles';
+import Posts from '../data/Posts';
 
 
 function Main() {
 
+    const titleField = '';
+
     return (
         <>
             <main>
-                <div className="container">
+                <div className="container debug">
+
+                    {/* FORM SECTION */}
+                    <section>
+                        <form action="" className='debug'>
+                            <h3 className='debug'>Insert Form</h3>
+
+                            <label htmlFor="postTitle">Title</label>
+                            <input type="text" className='debug' id='postTitle' />
+
+                        </form>
+                    </section>
+
+                    {/* POST SECTION */}
+                    <section>
+
+                    </section>
 
                 </div>
             </main >
